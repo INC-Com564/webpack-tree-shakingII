c5c6ab3a841678cef73cad73afbb0f45781e23e8
INC-Com739 <Iamisaiahcleveland@gmail.com>
2025-08-11 17:57:41 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+
+
+import { usefulFunction1, usefulVariable } from './myModule.js';
+
+usefulFunction1();
+console.log(usefulVariable);
+
---
+++ myModule.js
@@ -0,0 +1,17 @@
+// myModule.js
+
+export function usefulFunction1() {
+  console.log("This is useful function 1.");
+}
+
+export function usefulFunction2() {
+  console.log("This is useful function 2.");
+}
+
+export const usefulVariable = "A useful value.";
+
+export function unusedFunction() {
+  console.log("This function will not be imported or used.");
+}
+
+export const unusedVariable = "This variable will not be imported or used.";
